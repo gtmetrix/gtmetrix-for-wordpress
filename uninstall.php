8c70ac819543e9d1c8f1d40db45ed611d059e88e
Nicolas Demers <nicolas.demers@carbon60.com>
2021-03-03 16:05:44 -0800

    When uninstalling, delete all gfw_report and gfw_event posts

--- uninstall.php
+++ uninstall.php
@@ -6,4 +6,18 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 delete_option('gfw_options');
 
+// Delete reports and events
+$args = array(
+    'post_type' => array( 'gfw_report', 'gfw_event' ),
+    'post_status' => 'any',
+    'posts_per_page' => -1
+);
+
+$query = new WP_Query( $args );
+
+while ( $query->have_posts() ) {
+    $query->next_post();
+    wp_delete_post( $query->post->ID );
+}
+
 ?>
